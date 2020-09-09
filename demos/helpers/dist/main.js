e3c91b2eeb944b1d8382e95a1d5c2487ce027141
Amirouche <amirouche@hyper.dev>
2020-09-09 19:10:03 +0200

    demos/helpers: add it.
    ./demos/helpers/dist/helpers.js contains reactjs, for some reason
    there is no other way to access reactjs primitives without relying
    on npm, npx, and a trivial webpack config.
    
    Look into ./demos/helpers/dist/index.html to know how to use it.
    
    ref: https://webpack.js.org/guides/author-libraries/

---
+++ demos/helpers/dist/main.js
@@ -0,0 +1,8 @@
+let e = helpers.default.e;
+let ReactDOM = helpers.default.ReactDOM;
+
+
+let element = e("p", null, "hello world!");
+ReactDOM.render(element, document.getElementById("root"));
+
+
---
+++ demos/helpers/src/helpers.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+let e = React.createElement
+
+export default {
+    e,
+    ReactDOM
+}
