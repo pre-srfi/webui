935a3129ffad63e654f64542af4b793ff83c37b3
Amirouche <amirouche@hyper.dev>
2020-11-18 20:19:23 +0100

    xp++

--- demos/05-incoming/helpers.js
+++
@@ -1,1 +0,0 @@
-../00-helpers/dist/helpers.js
--- scm2js.sh
+++ scm2js.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -xe
+
 docker run -v $(pwd):/mnt --rm -it schemers/gambit:head gsc -target js -exe /mnt/$1
 tail -n +2 $(basename $1 .scm) > "$(basename $1 .scm).js"
 rm -f $(basename $1 .scm)
